4caddbc683de11f5198655ff10c9052d72453b15
Shuabhangi Jain <shubhangi.jain@nagarro.com>
2017-09-08 12:47:58 +0530

    Added enums and models

---
+++ CrossCuttingLayer/Enums/WaterMarkInfoFields.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileViewer.CrossCuttingLayer.Enums
+{
+    public enum WaterMarkInfoFields
+    {
+        IPAddress = 1,
+        EmailAddress= 2,
+        AccessDateTime=3,
+        PageNumberOutOfTotal=4,
+        CustomMessage=5
+    }
+}
---
+++ CrossCuttingLayer/Models/Watermark/WaterMarkInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileViewer.CrossCuttingLayer.Models.Watermark
+{
+    public class WaterMarkInfo
+    {
+        public string UserIPAddress { get; set; }
+
+        public string CustomMessage { get; set; }
+
+        public string Language { get; set; }
+
+    }
+}
